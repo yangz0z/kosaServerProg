a55146a21836c70bd4c3120173143157c90a2c18
yangz0z <gmlwls0522@gmail.com>
2023-04-20 10:19:22 +0900

    add route get or post

--- simpleWeb.py
+++ simpleWeb.py
@@ -16,6 +16,19 @@ def hi():
 def sample():
     return render_template('sample.html')
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    a = 0
+    b = 0
+    if request.method == 'GET' :
+        a = int(request.args.get('a'))
+        b = int(request.args.get('b'))
+    if request.method == 'POST' :
+        data = request.get_json() #flask에서 json으로 받기
+        a = int(data['a'])
+        b = int(data['b'])
+    return str(a+b)
+
 if __name__ == '__main__':
     app.run(
     host="127.0.0.1",
